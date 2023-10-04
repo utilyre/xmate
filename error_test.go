6f30343c671a45e5d9a1804c86758ccd137d8fa3
Amirabbas Fazelinia <utilyre@proton.me>
2023-10-04 10:17:44 +0330

    test(error): rewrite TestHTTPError_Is to cover more edge cases

--- error_test.go
+++ error_test.go
@@ -25,10 +25,33 @@ func TestNewHTTPError(t *testing.T) {
 }
 
 func TestHTTPError_Is(t *testing.T) {
-	err1 := NewHTTPError(http.StatusForbidden, "you do not have the privileges to access this endpoint")
-	err2 := NewHTTPError(http.StatusForbidden, "you are not allowed")
-
+	err1 := NewHTTPError(http.StatusConflict, "user already exists")
+	err2 := NewHTTPError(http.StatusConflict, "user already exists")
 	if !errors.Is(err1, err2) {
-		t.Error("err1 != err2; want err1 == err2")
+		t.Errorf("%#[1]v != %#[2]v; want %#[1]v = %#[2]v", err1, err2)
+	}
+
+	err1 = NewHTTPError(http.StatusForbidden, "you do not have the privileges to access this endpoint")
+	err2 = NewHTTPError(http.StatusForbidden, "you are not allowed")
+	if errors.Is(err1, err2) {
+		t.Errorf("%#[1]v = %#[2]v; want %#[1]v != %#[2]v", err1, err2)
+	}
+
+	err1 = NewHTTPError(http.StatusUnauthorized, "missing or malformed JWT")
+	err2 = NewHTTPError(http.StatusBadRequest, "missing or malformed JWT")
+	if errors.Is(err1, err2) {
+		t.Errorf("%#[1]v = %#[2]v; want %#[1]v != %#[2]v", err1, err2)
+	}
+
+	err1 = NewHTTPError(http.StatusInternalServerError, "something went wrong")
+	err2 = NewHTTPError(http.StatusNotFound, "page not found")
+	if errors.Is(err1, err2) {
+		t.Errorf("%#[1]v = %#[2]v; want %#[1]v != %#[2]v", err1, err2)
+	}
+
+	err1 = NewHTTPError(http.StatusMethodNotAllowed, "method POST is not allowed")
+	err2 = errors.New("method POST is not allowed")
+	if errors.Is(err1, err2) {
+		t.Errorf("%#[1]v = %#[2]v; want %#[1]v != %#[2]v", err1, err2)
 	}
 }
